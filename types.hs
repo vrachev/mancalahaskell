da340b12f5ff447537c1faa237962e951898f5af
vrachev96 <vrachev96@gmail.com>
2017-11-22 17:50:20 -0500

    types

--- types.hs
+++ types.hs
@@ -5,11 +5,23 @@ import qualified Data.List
 data Player = 
     Player { playerName :: String }
 
-data Cup p m = Cup { player :: Player
+data Cup p i m= Cup { player :: Player
+                   , cupIndex :: Int
                    , marbles :: Int }
 
-isCupEmpty :: (Cup a b) -> Bool
-isCupEmpty (Cup _ 0) = True
-isCupEmpty (Cup _ _) = False
+isCupEmpty :: (Cup p i m) -> Bool
+isCupEmpty (Cup _ _ 0) = True
+isCupEmpty (Cup _ _ _) = False
+
+marblesInCup :: (Cup p i m) -> Int
+marblesInCup (Cup _ _ n) = n
+
+instance Show Player where
+    show = playerName
+
+
+
+
+
 
 
