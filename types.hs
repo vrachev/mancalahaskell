ed5a9bcb21a1c0004aaa41b946d6ff7837e59587
vrachev96 <vrachev96@gmail.com>
2017-11-22 19:37:18 -0500

    types

--- types.hs
+++ types.hs
@@ -5,16 +5,27 @@ import qualified Data.List
 data Player = 
     Player { playerName :: String }
 
-data Cup p i m= Cup { player :: Player
-                   , cupIndex :: Int
-                   , marbles :: Int }
+-- Cup Player IndexOnBoard MarblesInCup 
+-- Mancala Player IndexOnBoard MarblesInMancala 
+data Tile = Cup Player Int Int 
+          | Mancala Player Int Int 
 
-isCupEmpty :: (Cup p i m) -> Bool
+-- data NextPlayerTurn = NextPlayerTurn { player :: Player }
+
+type Board = [Tile]
+
+-- data Board = Board { next :: NextPlayerTurn
+ --                  , cups :: List Cup }
+
+
+isCupEmpty :: Tile -> Bool
 isCupEmpty (Cup _ _ 0) = True
 isCupEmpty (Cup _ _ _) = False
+isCupEmpty (Mancala _ _ _) = error "Only call on cups, not mancala"
 
-marblesInCup :: (Cup p i m) -> Int
-marblesInCup (Cup _ _ n) = n
+marblesInTile :: Tile -> Int
+marblesInTile (Cup _ _ n) = n
+marblesInTile (Mancala _ _ n) = n 
 
 instance Show Player where
     show = playerName
