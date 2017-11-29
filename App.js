b835f4cb38c0f78caa52401d0ff0fa5c87ed781a
Marc Eastman <eastman@umd.edu>
2017-11-29 18:01:49 -0500

    Board looks better
    Find out how to say whose turn it is and change color of button depending on turn

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  handleClick1(){
-    console.log("click happened")
+  handleClick(buttonNum){
+    console.log(buttonNum)
   }
 
   render() {
     return (
-      <div>
-        <div>full array: {this.props.board}</div>
-        <button onClick = {this.handleClick1.bind(this)}></button>
+      <div className="board">
+
+        <div className="home">
+          {this.props.board[0]}
+        </div>
+        <div className="holes-section">
+
+          <div className="holes-container">
+            <button className = "hole" onClick = {()=>this.handleClick(13)}>{this.props.board[13]}</button>
+            <div className = "hole" onClick = {()=>this.handleClick(12)}>{this.props.board[12]}</div>
+            <div className = "hole" onClick = {()=>this.handleClick(11)}>{this.props.board[11]}</div>
+            <div className = "hole" onClick = {()=>this.handleClick(10)}>{this.props.board[10]}</div>
+            <div className = "hole" onClick = {()=>this.handleClick(9)}>{this.props.board[9]}</div>
+            <div className = "hole" onClick = {()=>this.handleClick(8)}>{this.props.board[8]}</div>
+          </div>
+
+
+
+        </div>
+
+        <div className="home">
+          { this.props.board[7] }
+        </div>
+
+
       </div>
     );
   }
