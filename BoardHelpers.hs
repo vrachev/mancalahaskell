5adc11918f45eec346a82f168343aa3ce20d2c65
vrachev96 <vrachev96@gmail.com>
2017-12-04 19:10:21 -0500

    deleted unnecessary folders

--- BoardHelpers.hs
+++
@@ -1,8 +0,0 @@
-module BoardHelpers where
-
-putIntoTwo :: Int -> String
-putIntoTwo n = if n < 10 then (show n) ++ " " else show n 
-
-if' :: Bool -> a -> a -> a
-if' True  x _ = x
-if' False _ y = y
